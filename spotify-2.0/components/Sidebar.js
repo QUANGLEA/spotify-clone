540eb18a15b8b4f119514a2ef36c7d3481b7242a
Quang Le <leanhquang2803@gmail.com>
2022-03-30 17:22:33 -0500

    Added nextauth and middleware

--- spotify-2.0/components/Sidebar.js
+++ spotify-2.0/components/Sidebar.js
@@ -6,41 +6,39 @@ import {
     HeartIcon,
     RssIcon
 } from "@heroicons/react/outline"
+import { signOut, useSession } from "next-auth/react"
 
 function Sidebar() {
+    const { data: session, status } = useSession();
     return (
-        <div className="text-gray-600 p-5 text-sm border-r
-        border-gray-900"> 
+        <div className="text-gray-600 p-5 text-sm border-r border-gray-900"> 
             <div className="space-y-3">
-                <button className="flex items-center space-x-2
-                hover:text-white">
+                <button className="flex items-center space-x-2 hover:text-white" onClick={() => signOut()}>
+                    <p>Log out</p>
+                </button>
+                <button className="flex items-center space-x-2 hover:text-white">
                     <HomeIcon className="h-5 w-5"/>
                     <p>Home</p>
                 </button>
-                <button className="flex items-center space-x-2
-                hover:text-white">
+                <button className="flex items-center space-x-2 hover:text-white">
                     <SearchIcon className="h-5 w-5"/>
-                    <p>Seach</p>
+                    <p>Search</p>
                 </button>
-                <button className="flex items-center space-x-2
-                hover:text-white">
+                <button className="flex items-center space-x-2 hover:text-white">
                     <LibraryIcon className="h-5 w-5"/>
                     <p>Your Library</p>
                 </button>
                 <hr className="border-t-[0.1] border-gray-900"/>
 
-                <button className="flex items-center space-x-2
-                hover:text-white">
+                <button className="flex items-center space-x-2 hover:text-white">
                     <PlusCircleIcon className="h-5 w-5"/>
                     <p>Create Playlist</p>
                 </button>
-                <button className="flex items-center space-x-2
-                hover:text-white">
+                <button className="flex items-center space-x-2 hover:text-white">
                     <HeartIcon className="h-5 w-5"/>
                     <p>Liked Songs</p>
                 </button>
-                <button className="flex items-center space-x-2
-                hover:text-white">
+                <button className="flex items-center space-x-2 hover:text-white">
                     <RssIcon className="h-5 w-5"/>
                     <p>Your Episodes</p>
                 </button>
