5c2d83cf1d2c0e440a700526582f5dc9bfbf44b5
Quang Le <leanhquang2803@gmail.com>
2022-03-31 18:57:20 -0500

    Added changes to _middleware.js

--- spotify-2.0/pages/_middleware.js
+++ spotify-2.0/pages/_middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
     // Token will exist if user is logged in 
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname } = req.nextUrl.clone();
+    const { pathname } = req.nextUrl;
     // Allow the requests if the following is true...
     //   1) It's a request for next-auth session & provider fetching
     //   2) The token exists
