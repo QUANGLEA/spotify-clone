dba3fd230b2e1dca1997d92ac58cafe5f72f829b
Quang Le <leanhquang2803@gmail.com>
2022-03-31 18:49:57 -0500

    Added changes to _middleware.js

--- spotify-2.0/pages/_middleware.js
+++ spotify-2.0/pages/_middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
     // Token will exist if user is logged in 
     const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
-    const { pathname } = req.nextUrl;
+    const { pathname } = req.nextUrl.clone();
     // Allow the requests if the following is true...
     //   1) It's a request for next-auth session & provider fetching
     //   2) The token exists
@@ -17,7 +17,8 @@ export async function middleware(req) {
 
     // Redirect them to login if they don't have token AND are requesting a protected route
     if (!token && pathname !== '/login') {
-        const url = "spotify-clone-2-d7ude5jwq-quanglea.vercel.app"
-        return NextResponse.redirect(url + "/login");
+        const url = req.nextUrl.clone();
+        url.pathname = "/login";
+        return NextResponse.redirect(url);
     }
 }
