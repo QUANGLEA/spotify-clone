e822785e1f42378e84da71d5e367c8c8b63c73d7
Quang Le <leanhquang2803@gmail.com>
2022-03-31 19:17:10 -0500

    Added changes to _middleware.js

--- spotify-2.0/pages/_middleware.js
+++ spotify-2.0/pages/_middleware.js
@@ -1,22 +1,18 @@
 import { getToken } from "next-auth/jwt";
-import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-    // Token will exist if user is logged in 
-    const token = await getToken({ req, secret: process.env.JWT_SECRET });
 
+export async function middleware(req) {
+    const url = req.nextUrl.clone();
+    const token = await getToken({ req, secret: process.env.JWT_SECRET});
     const { pathname } = req.nextUrl;
-    // Allow the requests if the following is true...
-    //   1) It's a request for next-auth session & provider fetching
-    //   2) The token exists
 
     if (pathname.includes('/api/auth') || token) {
         return NextResponse.next();
     }
-
-    // Redirect them to login if they don't have token AND are requesting a protected route
+    
     if (!token && pathname !== '/login') {
-        return NextResponse.redirect("http://localhost:3000/login");
+        url.pathname = '/login'
+        return NextResponse.redirect(url) 
     }
 }
